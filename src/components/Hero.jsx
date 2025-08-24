25ae59f40bb87d218d5d5b8a77aa03f5329db690
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-08-24 17:55:34 +0700

    Added Animations, Info Box, & Media Queries to Hero Page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,23 +8,23 @@ export const Hero = () => {
             <div className="hero-container">
                 <motion.div className="hero-content">
                     <motion.div className="headline">
-                        <h1 style={{color: "var(--accent-color)"}}>AI-Powered</h1>
-                        <h1>Skin Lesion Check</h1>
+                        <h1 className="glass-text">AI-Powered</h1>
+                        <h1 className="headline-2">Skin Lesion Check</h1>
                     </motion.div>
                     <h2 className="subtitle">Upload a photo of a mole or lesion and get instant AI-driven insights to support early skin health awareness.</h2>
                     <p className="note">
                         <i class="fas fa-exclamation-triangle" style={{color: "#f1c40f"}}></i> This tool does not provide a medical diagnosis. Please consult a dermatologist.
                     </p>
-                    <motion.a className="cta-btn">Check My Skin</motion.a>
+                    <motion.a className="cta-btn" whileHover={{scale: 1.05}} whileTap={{scale: 0.95}} transition={{duration: 0.2, ease: "easeIn"}}>Check My Skin</motion.a>
                 </motion.div>
 
                 <motion.div className="hero-image">
                     <img src="/attempt.jpg" alt="Face Image" />
-                    <motion.div className="info-box">
 
-                        {/* Semi-transparent background (rgba(0,0,0,0.5)) works well for floating over images.
-                        Rounded corners and subtle shadow to separate from the image.
-                        Use light text if background is dark, or dark text if the overlay is light. */}
+                    <motion.div className="info-box">
+                        <h1 className="template-title">Result Boolean</h1>
+                        <h3 className="template-subtitle">Disease Name</h3>
+                        <p className="template-desc">Description</p>
                     </motion.div>
                 </motion.div>
             </div>
