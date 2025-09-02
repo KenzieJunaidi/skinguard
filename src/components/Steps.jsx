979861164cdf386626d6d3bd035cd0abee668759
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-09-02 18:31:59 +0700

    Added Steps Page Content

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -5,14 +5,32 @@ export const Steps = () => {
         <motion.section id="steps" className="steps">
             <motion.div className="steps-content">
                 <motion.div className="steps-title">
-                    <motion.h3>3 Steps</motion.h3>
-                    <motion.h5>to keep your skin in check</motion.h5>
+                    <motion.h1>3 Steps</motion.h1>
+                    <motion.h3>TO KEEP YOUR SKIN IN CHECK</motion.h3>
                 </motion.div>
 
                 <motion.div className="cards-container">
-                    <motion.div></motion.div>
-                    <motion.div></motion.div>
-                    <motion.div></motion.div>
+                    <motion.div className="card">
+                        <img src="" alt="Card 1" />
+                        <motion.h3>Upload or Take a Photo</motion.h3>
+                        <motion.p>Choose an existing photo from your device or take a new one using your camera.</motion.p>
+                        <br></br>
+                        <motion.p>Make sure the skin area is clear, well-lit, and in focus.</motion.p>
+                    </motion.div>
+                    <motion.div className="card">
+                        <img src="" alt="Card 2" />
+                        <motion.h3>AI Analysis</motion.h3>
+                        <motion.p>Our AI model scans the image using advanced deep learning techniques.</motion.p>
+                        <br></br>
+                        <motion.p>It compares patterns against <item style={{fontWeight: 700}}>thousands</item> of known skin disease cases.</motion.p>
+                    </motion.div>
+                    <motion.div className="card">
+                        <img src="" alt="Card 3" />
+                        <motion.h3>Get Results & Insights</motion.h3>
+                        <motion.p>Know whether the condition looks benign <item style={{color: '#4CAF50'}}>safe</item> or potentially harmful.</motion.p>
+                        <br></br>
+                        <motion.p>You’ll also see possible disease categories (e.g., Acne, Melanoma, Psoriasis).</motion.p>
+                    </motion.div>
                 </motion.div>
             </motion.div>
         </motion.section>
