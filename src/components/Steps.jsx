938b8ece0659dd6c6d74638b7f957c89211a2066
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-09-03 20:34:38 +0700

    Added Card Animation

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -10,21 +10,21 @@ export const Steps = () => {
                 </motion.div>
 
                 <motion.div className="cards-container">
-                    <motion.div className="card">
+                    <motion.div className="card" whileHover={{ translateY: -10 }} transition={{ duration: 0.6 }}>
                         <i className="fas fa-cloud-arrow-up" style={{ fontSize: "5rem", color: "black" }}/>
                         <motion.h3>Upload or Take a Photo</motion.h3>
                         <motion.p>Choose an existing photo from your device or take a new one using your camera.</motion.p>
                         <br></br>
                         <motion.p>Make sure the skin area is clear, well-lit, and in focus.</motion.p>
                     </motion.div>
-                    <motion.div className="card">
+                    <motion.div className="card" whileHover={{ translateY: -10 }} transition={{ duration: 0.6 }}>
                         <i className="fas fa-brain" style={{ fontSize: "5rem", color: "black" }}/>
                         <motion.h3>AI Analysis</motion.h3>
                         <motion.p>Our AI model scans the image using advanced deep learning techniques.</motion.p>
                         <br></br>
                         <motion.p>It compares patterns against <item style={{fontWeight: 700}}>thousands</item> of known skin disease cases.</motion.p>
                     </motion.div>
-                    <motion.div className="card">
+                    <motion.div className="card" whileHover={{ translateY: -10 }} transition={{ duration: 0.6 }}>
                         <i className="fas fa-chart-line" style={{ fontSize: "5rem", color: "black" }}/>
                         <motion.h3>Get Results & Insights</motion.h3>
                         <motion.p>Know whether the condition looks benign <item style={{color: '#4CAF50'}}>safe</item> or potentially harmful.</motion.p>
