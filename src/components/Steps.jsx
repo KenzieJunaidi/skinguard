f19dd74100acd0e85c179477d8732495b61058fe
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-09-04 18:46:20 +0700

    Finalized Cards Hover Animations

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -11,6 +11,7 @@ export const Steps = () => {
 
                 <motion.div className="cards-container">
                     <motion.div className="card" whileHover={{ translateY: -10 }} transition={{ duration: 0.6 }}>
+                        <img src="/icon-background.png" alt="Icon Background"></img>
                         <i className="fas fa-cloud-arrow-up" style={{ fontSize: "5rem", color: "black" }}/>
                         <motion.h3>Upload or Take a Photo</motion.h3>
                         <motion.p>Choose an existing photo from your device or take a new one using your camera.</motion.p>
@@ -18,6 +19,7 @@ export const Steps = () => {
                         <motion.p>Make sure the skin area is clear, well-lit, and in focus.</motion.p>
                     </motion.div>
                     <motion.div className="card" whileHover={{ translateY: -10 }} transition={{ duration: 0.6 }}>
+                        <img src="/icon-background.png" alt="Icon Background"></img>
                         <i className="fas fa-brain" style={{ fontSize: "5rem", color: "black" }}/>
                         <motion.h3>AI Analysis</motion.h3>
                         <motion.p>Our AI model scans the image using advanced deep learning techniques.</motion.p>
@@ -25,6 +27,7 @@ export const Steps = () => {
                         <motion.p>It compares patterns against <item style={{fontWeight: 700}}>thousands</item> of known skin disease cases.</motion.p>
                     </motion.div>
                     <motion.div className="card" whileHover={{ translateY: -10 }} transition={{ duration: 0.6 }}>
+                        <img src="/icon-background.png" alt="Icon Background"></img>
                         <i className="fas fa-chart-line" style={{ fontSize: "5rem", color: "black" }}/>
                         <motion.h3>Get Results & Insights</motion.h3>
                         <motion.p>Know whether the condition looks benign <item style={{ fontWeight: 700, color: '#4CAF50'}}>safe</item> or potentially harmful.</motion.p>
