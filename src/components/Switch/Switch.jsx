2a5dfccd5ccabd2f336cfe95d4f03358505bbae1
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-09-06 17:27:18 +0700

    Added Switch Logic

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -1,10 +1,16 @@
 import './Switch.css';
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const Switch = () => {
+
+    const [isSwitch, setIsSwitch] = useState(false);
+
     return (
-        <motion.div className="container">
-            
+        <motion.div className={`container ${isSwitch ? "right" : ""}`} onClick={() => setIsSwitch(prev => !prev)}>
+            <motion.div className="circle">
+
+            </motion.div>
         </motion.div>
     );
 }
